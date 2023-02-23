c47feaab6723112f65e06ff0d00686e5312bb7d8
jakapong promsana <hicaruko@users.noreply.github.com>
2023-02-23 20:58:42 +0700

    Update flutter_elliptic_curve_keypair.dart
    add arguments

--- lib/flutter_elliptic_curve_keypair.dart
+++ lib/flutter_elliptic_curve_keypair.dart
@@ -12,7 +12,7 @@ class FlutterEllipticCurveKeyPair {
   /// return [EccResultModel] with [Signed Message] if success
   /// return [EccResultModel] with [Exception] if fail
   static Future<EccResultModel> ellipticCurveKeyPairSigning(
-      {String message, String language, String alias}) async {
+      {required String message, String? language, String? alias}) async {
     Map arguments = {
       "message": message,
       "language": language,
@@ -20,7 +20,7 @@ class FlutterEllipticCurveKeyPair {
     };
 
     try {
-      final String signedMessage =
+      final String? signedMessage =
           await _channel.invokeMethod('ellipticCurveKeyPairSigning', arguments);
       EccResultModel result = EccResultModel(
         data: signedMessage,
@@ -41,10 +41,43 @@ class FlutterEllipticCurveKeyPair {
   /// Get the public key from key pair
   /// return [EccResultModel] with [Publickey] if success
   /// return [EccResultModel] with [Exception] if fail
-  static Future<EccResultModel> ellipticCurveKeyPairPublicKey() async {
+  static Future<EccResultModel> ellipticCurveKeyPairPublicKey(
+      {String? alias}) async {
+    Map arguments = {
+      "alias": alias,
+    };
+
     try {
-      final String publicKey =
-          await _channel.invokeMethod('ellipticCurveKeyPairPublicKey');
+      final String? publicKey = await _channel.invokeMethod(
+          'ellipticCurveKeyPairPublicKey', arguments);
+      EccResultModel result = EccResultModel(
+        data: publicKey,
+        success: true,
+        description: "Public key recieved successfully",
+      );
+      return result;
+    } on PlatformException catch (e) {
+      EccResultModel platformException = EccResultModel(
+        data: e.details,
+        success: false,
+        description: e.message,
+      );
+      return platformException;
+    }
+  }
+
+  /// Get the public key from key pair
+  /// return [EccResultModel] with [Publickey] if success
+  /// return [EccResultModel] with [Exception] if fail
+  static Future<EccResultModel> ellipticCurveKeyPairPublicKeyPem(
+      {String? alias}) async {
+    Map arguments = {
+      "alias": alias,
+    };
+
+    try {
+      final String? publicKey = await _channel.invokeMethod(
+          'ellipticCurveKeyPairPublicKeyPem', arguments);
       EccResultModel result = EccResultModel(
         data: publicKey,
         success: true,
